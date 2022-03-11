319add39ed94cc81dc52769f8f2b608bb6fc2bbd
joshua <joshmarybrownonoda@gmail.com>
2022-03-11 10:45:12 +0100

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,13 +3,13 @@
 /**
  * main - Entry point
  *
- * Description: prints "with proper grammer, but the outcome is a piece of 
+ * Description: prints "with proper grammar, but the outcome is a piece of
  * art,"
  *
- * Return: ALways 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
