3cdf937eea2e3ee5389fceacaa1bb33906035dfc
joshua <joshmarybrownonoda@gmail.com>
2022-03-12 00:16:14 +0100

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,33 +1,40 @@
 #include <stdio.h>
 
 /**
- * main - print double digit combos
+ * main - print triple combos
  *
- * Description: print double digit combos
+ * Description: print triple digit combos
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int i, j;
+	int i, j, k;
 
 	i = 48;
 	j = 48;
+	k = 48;
 
 	while (i < 58)
 	{
 		j = i + 1;
 		while (j < 58)
 		{
-			putchar(i);
-			putchar(j);
+			k = j + 1;
+				while (k < 58)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i < 55 || j < 56 || k < 57)
+					{
+						putchar(44);
+						putchar(32);
+					}
+					k++;
+				}
 
-			if (i < 56 || j < 57)
-			{
-				putchar(44);
-				putchar(32);
-			}
 			j++;
 		}
 
