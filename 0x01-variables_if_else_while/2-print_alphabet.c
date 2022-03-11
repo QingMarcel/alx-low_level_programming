bb7e86c2d17e222abae135fb329e8549492c5981
joshua <joshmarybrownonoda@gmail.com>
2022-03-11 20:06:44 +0100

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 /**
  * main - Entry point
+ *
  * Description: print all small letters
+ *
  * Return 0 (Success)
 */
 
 int main(void)
 {
 	char alpha;
+
 	for (alpha = 'a'; alpha <= 'z'; ++alpha)
 	{
-		putchar(alpha);
+		putchar("%c\n",alpha);
 	}
 	return (0);
 
