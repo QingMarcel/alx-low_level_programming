c4c440f603e89e054696da6d5f4985a6bd7a9b79
joshua <joshmarybrownonoda@gmail.com>
2022-03-11 10:18:58 +0100

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - Entry Point
+*
+*Desciption: prints "Programming is like building a multiple puzzle"
+*
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multiple puzzle");
+	return (0);
+}
