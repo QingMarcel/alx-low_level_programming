de08d18b9fe619928a2a934ffe954f277aa54acb
joshua <joshmarybrownonoda@gmail.com>
2022-03-16 13:14:39 +0100

    Lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print: print alphabet in lowercase
+ *
+ * Return: void
+ */
+
+void print_lowercase(void)
+{
+	int i;
+	for (i = 'a'; i < 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
